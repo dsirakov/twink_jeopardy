022b00f38baec5d23034e7c5850ac669695a747d
Daniel Sirakov <daniel.sirakov@nielseniq.com>
2025-12-16 17:10:36 +0200

    Update test

--- tests/test_questions.py
+++ tests/test_questions.py
@@ -12,7 +12,7 @@ from app.db.models import Question
 from datetime import date
 
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./tests/test.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
